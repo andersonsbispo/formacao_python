d7f6eb1d14f075d5619f71ad19e59502cbf272aa
Anderson Bispo <andersonsbispo1997@outlook.com>
2024-08-20 23:07:25 -0300

    Inserção de licensa

---
+++ Modulo_1/tipos_variaveis/lista.py
@@ -0,0 +1,23 @@
+# Declaração
+
+minha_lista = [1, 2, 3, 4, 5, 'rocketseat', True, False]
+
+# Exibindo a lista
+print('Minha lista de exemplo:', minha_lista)
+
+# Exibindo item da lista
+minha_lista[0] = 'python'
+print('Minha lista de exemplo:', minha_lista)
+print('minha_lista[0]:', minha_lista[0])
+print('minha_lista[5]:', minha_lista[5])
+print('minha_lista[1:7]:' ,minha_lista[1:7])
+print('minha_lista[:6]:' ,minha_lista[:6])
+print('minha_lista[2:]:' ,minha_lista[2:])
+
+# Métodos de lista
+
+# Método append(): Adiciona um elemento ao final da lista
+minha_lista.append(6)
+print('Após append(6):', minha_lista)
+
+# Médoto index
